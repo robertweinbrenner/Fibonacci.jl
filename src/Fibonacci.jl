85cd8df10e4b27836697794853853fc880e45268
Robert Weinbrenner <robert.weinbrenner@t-online.de>
2024-08-09 12:47:45 +0200

    Initial Commit

--- src/Fibonacci.jl
+++ src/Fibonacci.jl
@@ -1,5 +1,20 @@
+# https://gist.github.com/t-nissie/641df996b9035f85b230?
+# permalink_comment_id=2211339#gistcomment-2211339
+
 module Fibonacci
-
-# Write your package code here.
-
-end
+export fibonacci
+fibonacci(n :: Int) = fibonacci(BigInt(n))
+function fibonacci(n::BigInt)
+    if n == 0
+    return (BigInt(0),BigInt(1))
+    else
+        a, b = fibonacci(div(n, 2))
+        c = a * (b *BigInt(2)-a)
+        d = a*a+b*b
+        if iseven(n)
+            return (c,d)
+        else
+            return (d, c + d)
+        end
+end end
+end # module Fibonacci
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,7 @@ using Fibonacci
 using Test
 
 @testset "Fibonacci.jl" begin
-    # Write your tests here.
+    @test fibonacci(0) == (0,1)
+    @test fibonacci(1) == (1,1)
+    @test fibonacci(30) == (832040,1346269)
 end
